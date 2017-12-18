33e95b6c3aab4d36533c83b1801337bb08dd9bc6
Darren Vong <d.vongy@gmail.com>
2017-12-18 23:43:01 +0000

    Day 13 part 2 & made overall solution more compact (efficient in part 1...)

--- day13.py
+++ day13.py
@@ -8,44 +8,45 @@ the layer the packet is being caught multiplied by the layer's range
 
 Part 2: What's the minimum delay such that the packet won't get caught?
 """
-from collections import defaultdict
 from itertools import count
 
-def calculate_severity():
-    states = defaultdict(lambda: {"range": 0, "scanner_pos": -1, "dir": 1})
-    with open("day13_input.txt") as f:
-        for line in f:
-            line = line.split(": ")
-            # states stores the tuple (range_of_layer, scanner_pos)
-            states[int(line[0])] = {"range": int(line[1]), "scanner_pos": 0, "dir": 1}
+def get_scanner_pos(ps, depth, firewall):
+    return ps % ((firewall[depth] - 1) * 2) if depth in firewall else -1
 
-    total_picosecs = max(states) + 1
+def calculate_severity(firewall):
+    total_picosecs = max(firewall) + 1
     severity = 0
-    packet_pos = -1
+
     for ps in range(total_picosecs):
-        packet_pos += 1
-
         # scanner is there as packet moves in
-        if states[packet_pos]["scanner_pos"] == 0:
+        if get_scanner_pos(ps, ps, firewall) == 0:
             # so packet caught which adds to severity score
-            severity += ps * states[ps]["range"]
-
-        # all scanners move by 1
-        for depth in states:
-            # if moving ahead will move scanner out of range...
-            if (states[depth]["scanner_pos"] + states[depth]["dir"] == states[depth]["range"] or
-                    states[depth]["scanner_pos"] + states[depth]["dir"] == -1):
-                # then reverse dir first before moving it below
-                states[depth]["dir"] *= -1
-
-            states[depth]["scanner_pos"] += states[depth]["dir"]
+            severity += ps * firewall[ps]
 
     return severity
 
+def get_min_delay(firewall):
+    total_picosecs = max(firewall) + 1
+
+    for delay in count(start=1):
+        try:
+            for ps in range(delay, total_picosecs + delay):
+                # packet is caught
+                if get_scanner_pos(ps, ps - delay, firewall) == 0:
+                    raise StopIteration
+            return delay
+        except StopIteration:
+            continue
+
 if __name__ == '__main__':
     print("=" * 15, "Part 1", "=" * 15)
-    print(f"The severity score (if packet moves as fast as it can) is {calculate_severity()}")
+    firewall = {}
+    with open("day13_input.txt") as f:
+        for line in f:
+            depth, _range = line.split(": ")
+            firewall[int(depth)] = int(_range)
+    print(f"The severity score is {calculate_severity(firewall)}")
     print()
 
     print("=" * 15, "Part 2", "=" * 15)
-    # print()
+    print(f"The min picoseconds to delay w/o getting caught is {get_min_delay(firewall)}")
