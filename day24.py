703ffc509ecc733f92d5b128f4af085796702478
Darren Vong <d.vongy@gmail.com>
2017-12-29 20:45:17 +0000

    Further back up on day 24, still not working (!!!) but seems to be making small progress

--- day24.py
+++ day24.py
@@ -7,40 +7,52 @@ Part 1: ??
 
 Part 2: ??
 """
+from collections import deque
+class TrieNode:
 
-def generate_bridge(components, seen, bridge=None):
+    def __init__(self, val, children):
+        self.val = val # int
+        self.children = children # dict<tuple, TrieNode>
+
+def generate_bridge(components, trie: TrieNode, l_prev=0, r_prev=0):
     for l_port, r_port in components:
-        bridge = bridge or []
-        if not bridge: # bridge is empty
-            if l_port == 0 or r_port == 0:
-                bridge.append((l_port, r_port))
-                seen.append((bridge, sum(l + r for l, r in bridge)))
-                generate_bridge(components, seen, bridge)
-                bridge = []
-        elif not ((l_port, r_port) in bridge or (r_port, l_port) in bridge):
-            unused_port = bridge[-1][1]
-            if l_port == unused_port:
-                new_bridge = bridge + [(l_port, r_port)]
-                if new_bridge not in seen:
-                    seen.append((new_bridge, sum(l + r for l, r in new_bridge)))
-                    generate_bridge(components, seen, new_bridge)
-            elif r_port == unused_port:
-                new_bridge = bridge + [(r_port, l_port)]
-                if new_bridge not in seen:
-                    seen.append((new_bridge, sum(l + r for l, r in new_bridge)))
-                    generate_bridge(components, seen, new_bridge)
-    return seen
+        if trie.val == 0:
+            if l_port == 0:
+                _next = TrieNode(sum((l_port, r_port)), {})
+                trie.children[l_port, r_port] = _next
+                generate_bridge(components - {(l_port, r_port)}, _next, l_port, r_port)
+        else:
+            # l_prev, r_prev = trie.val
+            if (l_port == r_prev or r_port == r_prev or
+                    l_port == l_prev or r_port == l_prev):
+                _next = TrieNode(sum((l_port, r_port), trie.val), {})
+                trie.children[l_port, r_port] = _next
+                generate_bridge(
+                    components - {(l_port, r_port)}, _next, l_port, r_port)
 
-def get_strongest_bridge(bridges):
-    return max(bridges, key= lambda x: x[1])[1]
+def get_strongest_bridge(trie):
+    # try depth first search and if I'm still stick then
+    # I'll throw the towel in for a bit...
+    pass 
+
+def calculate_longest_strength(trie):
+    if not trie.children:
+        return sum(trie.val)
+    else:
+        total = 0
+        for c, c_node in trie.children.items():
+            total += calculate_longest_strength(c_node)
+        return total
+
+# def get_strongest_bridge(bridges):
+#     return max(bridges, key= lambda x: x[1])[1]
 
 if __name__ == '__main__':
     print("=" * 15, "Part 1", "=" * 15)
-    with open("day24_input.txt") as f:
-        components = [tuple(map(int, line.strip().split("/"))) for line in f]
-    all_bridges = generate_bridge(components, [])
-    print(f"Strongest bridge has strength {get_strongest_bridge(all_bridges)}")
-    print()
+    with open("day24_sample.txt") as f:
+        components = {tuple(map(int, line.strip().split("/"))) for line in f}
+    root_trie = TrieNode(0, {})
+    generate_bridge(components, root_trie)
 
     print("=" * 15, "Part 2", "=" * 15)
     # print()
